1e8968cce5322cde32c81b2f3af30e81b848f57b
Aishwarya <aishwarya04ashok@gmail.com>
2023-07-23 00:43:53 +0530

    Day 2

--- day1/lib/main.dart
+++ day1/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:day1/Pages/home_page.dart';
 import "package:flutter/material.dart";
+
+import 'Pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,8 +13,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Material(
-            child: Center(
-                child: Container(child: Text('Meri yahan pe kilas rhi hai')))));
-  }
+        home:HomePage(),
+        themeMode:ThemeMode.dark,  //one way
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        theme:ThemeData(          //other way
+          primarySwatch: Colors.amber,
+        ),
+        routes: {
+          "/": (context)=>HomePage(),
+          "/login_page": (context)=>LoginPage(),
+        }
+    );
+    
+}
 }
